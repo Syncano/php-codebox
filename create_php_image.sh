48f18c36a03f621f0aa3996254078e21a4f5569d
Zhe Brah <zhebrah@gmail.com>
2016-10-04 18:27:18 +0200

    [INFRA-317] rewrite to use Ansible

---
+++ create_php_image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $1 != "--partial" ]
+then
+    docker build -t quay.io/syncano/php-codebox .
+fi
+
+docker run -d -p 2200:22 --env-file image/environment --name ansible quay.io/syncano/php-codebox dumb-init /usr/sbin/sshd -D
+ansible-playbook -i "`docker inspect --format '{{ .NetworkSettings.IPAddress }}' ansible`," -u root --private-key image/id_rsa provision-php-codebox.yml
+docker commit ansible quay.io/syncano/php-codebox
+
+docker run -d --env-file image/environment -u syncano --name php quay.io/syncano/php-codebox dumb-init /usr/sbin/sshd -D
+docker commit php quay.io/syncano/php-codebox
